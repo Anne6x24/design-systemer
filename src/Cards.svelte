1858f87712c26f57e2d2894f8ab11f6f9e4cc627
“Anne6x24” <Anne6x24@stud.kea.dk>
2022-03-11 11:34:38 +0100

    first commit

---
+++ src/Cards.svelte
@@ -0,0 +1,50 @@
+
+<script>
+    import Button from "./Button.svelte"
+    import Badge from "./Badge.svelte"
+</script>
+
+
+<section class="card">
+<img src="./stolthed.png" alt="">
+<div class="flex">
+<h3>Shoes! <Badge type="secondary">NEW</Badge></h3>
+
+</div>
+<p>If a dog chewes shoes whoes shoes does he choose?</p>
+<Badge type="ghost_border">Fashion</Badge>
+<Badge type="ghost_border">Products</Badge>
+<div class="blue">
+<Button type="primary">BUY NOW</Button>
+</div>
+</section>
+
+<style>
+   .card {
+       width: 25rem;
+       border-radius: 10px 10px 10px 10px;
+       box-shadow: 2px 10px darkgrey;
+       
+       
+   }
+   
+   img {
+       max-width: 25rem;
+       border-radius: 15px 15px 0px 0px;
+   }
+   .flex {
+       display: flex;
+   }
+
+   h3 {
+       margin-right: 1rem;
+   }
+
+   .blue {
+       margin-left: 19rem;
+   }
+
+   
+
+</style>
+
---
+++ src/Inbox.svelte
@@ -0,0 +1,30 @@
+<script>
+        import Button from "./Button.svelte"
+        import Badge from "./Badge.svelte"
+       
+</script>
+
+
+<div>
+    <div class="button">
+<Button>INBOX</Button>
+</div>
+<div class="badge">
+<Badge type="secondary">99+</Badge>
+</div>
+</div>
+
+
+<style>
+
+.button {
+position: relative;
+}
+
+.badge {
+position: absolute;
+top: 125.4rem;
+left: 25.7rem;
+}
+
+</style>
